e089c95148070410c203023dee986664b213f9db
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-26 15:10:39 +0500

    Update ping_pong.

--- ooter_game.py
+++ ooter_game.py
@@ -1,10 +1,9 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        sprite.Sprite.__init__(self)    
-
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,55 +12,44 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    def update(self):
+class Player(GameSprite): # класс ракетки
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5: # вниз
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 620: # наверх
-            self.rect.y += speed 
+        if keys[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
 
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5: # вниз
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 620: # наверх
-            self.rect.y += speed 
+        if keys[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
 
 back = (200, 255, 255)
-window = display.set_mode((600,500))
+window = display.set_mode((600, 500))
+display.set_caption('Пинг-понг')
 window.fill(back)
 
 game = True
-finish = False
-clock = clock.Clock()
+clock = time.Clock()
+FPS = 60
 
-# 1 мяч, 2 палки
-racket1 = Player('racket.png', 30, 200, 4, 50, 150)
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tennis_ball.png', 200, 200, 4, 50, 50)
-
-font.init()
-font = font.Font(None, 35)
-lose1 = font.render( '1 ИГРОК ЛОХ', True, (180, 0, 0))
-lose2 = font.render( '2 ИГРОК ЛОХ', True, (180, 0, 0))
-
-speed_x = 3
-speed_y = 3
+racket1 = Player('Coin.png', 30, 200, 4, 50, 150) # создание ракеток и мяча
+racket2 = Player('Coin.png', 520, 200, 4, 50, 150)
+ball = GameSprite('Heart.png', 275, 225, 4, 50, 50)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    if finish != True:
-        window.fill()
-        racket1.update_l()
-        racket2.update_r()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
+    window.fill(back)
+    racket1.update_l()
+    racket2.update_r()
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
 
-        racket1.reset()
-        racket2.reset()
-        ball.reset()
     display.update()
-    closk.tick(60)
+    clock.tick(FPS)
